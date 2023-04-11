3d902ebe84dbccaf42f4ae35d4a2490b48f28c6f
den59k <den59k@gmail.com>
2023-04-11 18:07:00 +0500

    feat: init commit

---
+++ src/index.ts
@@ -0,0 +1,91 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import { Context, Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters'
+import { InworldClient, InworldConnectionService, InworldPacket } from '@inworld/nodejs-sdk';
+
+if (!process.env.BOT_TOKEN) {
+  throw new Error("BOT_TOKEN is not defined")
+}
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+const conversations = new Map<number, InworldConnectionService>()
+  
+
+const createConversation = (ctx: Context) => {
+  const client = new InworldClient()
+  client.setApiKey({
+    key: process.env.INWORLD_KEY!,
+    secret: process.env.INWORLD_SECRET!
+  })
+  client.setConfiguration({
+    capabilities: { audio: false, emotions: false }
+  })
+  const name = ctx.from?.first_name || ctx.from?.last_name || ctx.from?.username
+  if (name) {
+    client.setUser({ fullName: name })
+  }
+  client.setScene(process.env.INWORLD_SCENE!)
+
+  const chatId = ctx.chat!.id
+  client.setOnError((err: Error) => {
+    console.log(`Close conversation with chatId ${ chatId }. Reason: ${ err.message }`)
+    conversations.delete(chatId)
+  })
+  client.setOnMessage((packet: InworldPacket) => {
+    if (!packet.text) {
+      return
+    }
+    console.log(`Sended reply to chatId ${ chatId }`)
+    bot.telegram.sendMessage(chatId, packet.text.text.trim())
+  })
+  const connection = client.build()
+  conversations.set(chatId, connection)
+  console.log(`Create conversation with chatId ${ chatId }`)
+
+  return connection
+}
+
+const stop = () => {
+  bot.stop('SIGTERM')
+  for (let connection of conversations.values()) {
+    if (connection.isActive()) {
+      connection.close()
+    }
+  }
+}
+
+const init = async () => {
+  bot.start((ctx) => {
+    let conversation = conversations.get(ctx.chat.id)
+    if (!conversation) {
+      conversation = createConversation(ctx)
+    }
+    conversation.sendText("Hello! Please introduce yourself")
+  });
+
+  bot.on(message("text"), async (ctx) => {
+    if (ctx.message.chat.type !== "private") return
+    let conversation = conversations.get(ctx.chat.id)
+    if (!conversation) {
+      conversation = createConversation(ctx)
+    }
+    console.log(`Receive message from chatId ${ ctx.chat.id }`)
+    conversation.sendText(ctx.message.text)
+  })
+
+  bot.launch();
+
+  console.log("Bot launched!")
+
+  // Enable graceful stop
+  process.once('SIGINT', () => {
+    stop()
+  });
+  process.once('SIGTERM', () => {
+    stop()
+  });
+}
+
+init()
